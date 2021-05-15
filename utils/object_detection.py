523de3fecc9999e467231f84c53cb189d37c4570
Nave Itay <59213562+NaveItay@users.noreply.github.com>
2021-05-15 22:52:42 +0300

    Add files via upload

--- utils/object_detection.py
+++ utils/object_detection.py
@@ -4,47 +4,60 @@ import time
 
 CONFIDENCE_THRESHOLD = 0.6
 NMS_THRESHOLD = 0.1
-COLORS = [(0, 100, 255) , (100, 255, 255), (255, 255, 0)]
+COLORS = [(255, 0, 0), (0, 0, 255), (0, 0, 255)]
 
 
-class ObjectDetection():
-
+class ObjectDetection:
     class_names = []
     model = None
 
     def initialize_model(self):
 
-        with open("/home/chameleonvision/Desktop/Project/model/volley.names", "r") as f:
+        # with open("/home/chameleonvision/Desktop/Project/model/volley.names", "r") as f:
+        #     self.class_names = [cname.strip() for cname in f.readlines()]
+        with open('volley.names', 'r') as f:
             self.class_names = [cname.strip() for cname in f.readlines()]
 
-        net = cv2.dnn.readNet("/home/chameleonvision/Desktop/Project/model/volley.weights", "/home/chameleonvision/Desktop/Project/model/volley.cfg")
+        # net = cv2.dnn.readNet("/home/chameleonvision/Desktop/Project/model/volley.weights", "/home/chameleonvision/Desktop/Project/model/volley.cfg")
+        net = cv2.dnn.readNet('volley.weights',
+                              'volley.cfg')
+
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
         net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
         self.model = cv2.dnn_DetectionModel(net)
-        self.model.setInputParams(1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
-
+        self.model.setInputParams(1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
 
     def detect(self, frame):
         self.classes, scores, boxes = self.model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
         return self.classes, scores, boxes
 
-
     def draw_objects(self, frame, classes, scores, boxes, field_center):
 
         for (classid, score, box) in zip(classes, scores, boxes):
-            
+
             color = COLORS[2]
-            if(classid[0] == 0) :
-                if(get_box_center(box)[0] < field_center) : color = COLORS[0]
-                else : color = COLORS[1]
-                
+            if classid[0] == 0:
+                if get_box_center(box)[0] < field_center:
+                    color = COLORS[0]
+                else:
+                    color = COLORS[1]
+
             # color = COLORS[int(classid) % len(COLORS)]
             label = "(%d, %d)" % (get_box_center(box)[0], get_box_center(box)[1])
             overlay = frame.copy()
-            cv2.rectangle(overlay, box, color, -1)
-            cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
-            cv2.putText(frame, label, (box[0] , box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
-            
-        return frame
 
+            # Draw player
+            if classid[0] == 0:
+                cv2.rectangle(overlay, box, color, -1)
+                cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
+                cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+            # Draw ball
+            if classid[0] == 1:
+                ballMidpoint = (get_box_center(box)[0], get_box_center(box)[1])
+
+                cv2.circle(overlay, ballMidpoint, int(box[2]/2),  color, 2)
+                cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
+                cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+        return frame
